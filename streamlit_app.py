9075b7b73d5d0921ed4f0cc9930b3b520510edb0
fayyazzr <136173600+fayyazzr@users.noreply.github.com>
2025-05-21 14:00:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,21 +5,4 @@ st.set_page_config(page_title="Profil Prabowo Subianto")
 st.title("Profil Prabowo Subianto")
 
 
-st.image(
-    "https://upload.wikimedia.org/wikipedia/commons/6/6f/Prabowo_Subianto_2014.jpg",
-    caption="Prabowo Subianto",
-    use_column_width=True
-)
-
-
-st.markdown("""
-**Prabowo Subianto Djojohadikusumo** adalah seorang tokoh militer dan politikus Indonesia.  
-Ia menjabat sebagai **Menteri Pertahanan Republik Indonesia** sejak tahun 2019.  
-Prabowo adalah ketua umum Partai Gerindra dan merupakan mantan Komandan Jenderal Kopassus.
-
-### Fakta Singkat:
-- **Lahir**: 17 Oktober 1951
-- **Pendidikan**: Akademi Militer Magelang (1974)
-- **Partai Politik**: Partai Gerakan Indonesia Raya (Gerindra)
-- **Jabatan Saat Ini**: Menteri Pertahanan RI
-""")
+st.image(images (29).jpg)
