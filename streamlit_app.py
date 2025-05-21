b67c4cec23d42eee3682e062bb376b5ed3f8a104
fayyazzr <136173600+fayyazzr@users.noreply.github.com>
2025-05-21 14:08:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("Profil Prabowo Subianto")
-
-
-st.image("images (29).jpg", width 200)
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
+st.image("IMG_20250513_102151.jpg", width=200)
