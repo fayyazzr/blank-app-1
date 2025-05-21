db3741cdd837d68a545c3a3b7b38f81b4912888a
fayyazzr <136173600+fayyazzr@users.noreply.github.com>
2025-05-21 13:33:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
+import streamlit as st
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+
+st.title("Prabowo Subianto")
+
+st.image(
+    "https://upload.wikimedia.org/wikipedia/commons/6/6f/Prabowo_Subianto_2014.jpg",
+    caption="Prabowo Subianto",
+    use_column_width=True
+)
+
+st.write("""
+Prabowo Subianto adalah seorang tokoh politik dan militer Indonesia. Saat ini, ia menjabat sebagai Menteri Pertahanan Republik Indonesia.
+""")
