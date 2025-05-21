8f1f203d29cf6f7cdd351004ef8d13b74dd55f0a
fayyazzr <136173600+fayyazzr@users.noreply.github.com>
2025-05-21 17:27:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Profil Prabowo Subianto")
 
 st.image("Prabowo_Subianto_2024_official_portrait.jpg", width=200)
 
-st.title("Prabowo Subianto Djojohadikusumo ( EYD : Joyohadikusumo ; lahir 17 Oktober 1951) adalah seorang politikus, pengusaha, dan mantan jenderal bintang empat yang menjabat sebagai presiden Indonesia kedelapan sejak 2024. Sebelumnya, ia adalah menteri pertahanan ke-26 di bawah presiden Joko Widodo dari 2019 hingga 2024. Prabowo adalah presiden Indonesia ketiga yang memiliki latar belakang militer setelah Suharto dan Susilo Bambang Yudhoyono dan merupakan presiden tertua yang menjabat pertama kali dalam sejarah Indonesia.")
+st.title("Prabowo Subianto Djojohadikusumo lahir 17 Oktober 1951) adalah seorang politikus, pengusaha, dan mantan jenderal bintang empat yang menjabat sebagai presiden Indonesia kedelapan sejak 2024. Sebelumnya, ia adalah menteri pertahanan ke-26 di bawah presiden Joko Widodo dari 2019 hingga 2024. Prabowo adalah presiden Indonesia ketiga yang memiliki latar belakang militer setelah Suharto dan Susilo Bambang Yudhoyono dan merupakan presiden tertua yang menjabat pertama kali dalam sejarah Indonesia.")
